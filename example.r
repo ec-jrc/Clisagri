cd05f9e10cd33e6a833c4b694bc905ad4b97834e
ceglaan <64787402+ceglaan@users.noreply.github.com>
2020-05-22 11:18:38 +0200

    Update example.r

--- example.r
+++ example.r
@@ -1,8 +1,8 @@
 ### EXAMPLE OF USE - RAVENNA
-source("clisagri.r")
+source("R/clisagri.r")
 
 #read meteo data
-meteo = read.csv("MeteoRavenna.csv", sep=",", header = TRUE)
+meteo = read.csv("data/MeteoRavenna.csv", sep=",", header = TRUE)
 # convert dates in meteo to class Date
 meteo$DAY = as.Date(meteo$DAY, format="%d/%m/%Y")
 
@@ -10,17 +10,17 @@ meteo$DAY = as.Date(meteo$DAY, format="%d/%m/%Y")
 ravenna = clisagri(meteo, lat = 41.5, drought.coef=NULL)
 
 ######### calculate indicators based on development stages DVS #########
-source("phenology.r")
-parameters = read.csv("ParametersRavenna.csv", header=TRUE, sep=",")
-sowing = read.csv("SowingRavenna.csv", header=TRUE, sep=",")
+source("R/phenology.r")
+parameters = read.csv("data/ParametersRavenna.csv", header=TRUE, sep=",")
+sowing = read.csv("data/SowingRavenna.csv", header=TRUE, sep=",")
 sowing$DAY = as.Date(sowing$DAY, format="%d/%m/%Y")
 meteo = phenology(meteo, parameters, sowing$DAY, 41.5)
 meteo$DVS = DVS2BBCH(meteo$DVS)
 ravenna.dvs = clisagri(meteo,41.5,sowing=sowing$DAY)
 
 ######## phenological simulation tailored for breeders #########
-source("phenology.breeder.r")
-parameters = read.csv("ParametersRavenna.breeder.csv", header=TRUE, sep=",")
+source("R/phenology.breeder.r")
+parameters = read.csv("data/ParametersRavenna.breeder.csv", header=TRUE, sep=",")
 meteo = phenology.breeder(meteo, parameters, sowing$DAY, 41.5)
 meteo$DVS = DVS2BBCH(meteo$DVS, dvs.end = 6)
 ravenna.dvs = clisagri(meteo,41.5,sowing=sowing$DAY)
@@ -28,10 +28,10 @@ ravenna.dvs = clisagri(meteo,41.5,sowing=sowing$DAY)
 
 ##########  Figure 2 in manuscript ####### 
 #### development stage simulation using standard phenology model ######
-source("hazard.map.r")
+source("R/hazard.map.r")
 
-source("phenology.r")
-parameters = read.csv("ParametersRavenna.csv", header=TRUE, sep=",")
+source("R/phenology.r")
+parameters = read.csv("data/ParametersRavenna.csv", header=TRUE, sep=",")
 
 dvs.ravenna = dvs.calc(meteo=meteo.ravenna, sowing="2002-10-30", parameters=parameters, latitude=44.42, r.tsum1=seq(600,1000,by=20), r.tsum2=seq(600, 1000, by=20), parallel=TRUE, ncores=10)
 
