e8629b25ca00be74113011e1f827ec6f9da30c83
ceglaan <64787402+ceglaan@users.noreply.github.com>
2020-05-22 10:35:27 +0200

    Add files via upload

---
+++ example.r
@@ -0,0 +1,38 @@
+### EXAMPLE OF USE - RAVENNA
+source("agro.clim.r")
+
+#read meteo data
+meteo = read.csv("MeteoRavenna.csv", sep=",", header = TRUE)
+# convert dates in meteo to class Date
+meteo$DAY = as.Date(meteo$DAY, format="%d/%m/%Y")
+
+######### calculate all indicators for fixed period s#########
+ravenna = agro.clim(meteo, lat = 41.5, drought.coef=NULL)
+
+######### calculate indicators based on development stages DVS #########
+source("phenology.r")
+parameters = read.csv("ParametersRavenna.csv", header=TRUE, sep=",")
+sowing = read.csv("SowingRavenna.csv", header=TRUE, sep=",")
+sowing$DAY = as.Date(sowing$DAY, format="%d/%m/%Y")
+meteo = phenology(meteo, parameters, sowing$DAY, 41.5)
+meteo$DVS = DVS2BBCH(meteo$DVS)
+ravenna.dvs = agro.clim(meteo,41.5,sowing=sowing$DAY)
+
+######## phenological simulation tailored for breeders #########
+source("phenology.breeder.r")
+parameters = read.csv("ParametersRavenna.breeder.csv", header=TRUE, sep=",")
+meteo = phenology.breeder(meteo, parameters, sowing$DAY, 41.5)
+meteo$DVS = DVS2BBCH(meteo$DVS, dvs.end = 6)
+ravenna.dvs = agro.clim(meteo,41.5,sowing=sowing$DAY)
+
+
+##########  Figure 2 in manuscript ####### 
+#### development stage simulation using standard phenology model ######
+source("hazard.map.r")
+
+source("phenology.r")
+parameters = read.csv("ParametersRavenna.csv", header=TRUE, sep=",")
+
+dvs.ravenna = dvs.calc(meteo=meteo.ravenna, sowing="2002-10-30", parameters=parameters, latitude=44.42, r.tsum1=seq(600,1000,by=20), r.tsum2=seq(600, 1000, by=20), parallel=TRUE, ncores=10)
+
+
