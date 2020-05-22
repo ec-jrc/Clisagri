3a2b4081d9f4f71fc011d0521286d74b65598662
ceglaan <64787402+ceglaan@users.noreply.github.com>
2020-05-22 11:21:48 +0200

    Update phenology.parameters.r

--- R/phenology.parameters.r
+++ R/phenology.parameters.r
@@ -1,7 +1,7 @@
 phenology.parameters = function(breeder=FALSE)
 {
   if(!breeder)
-    print(read.table(file="PARAM.DESC.csv", header = TRUE, sep=";"))
+    print(read.table(file="data/PARAM.DESC.csv", header = TRUE, sep=";"))
   else
-    print(read.table(file="PARAM.DESC.breeder.csv", header = TRUE, sep=";"))
+    print(read.table(file="data/PARAM.DESC.breeder.csv", header = TRUE, sep=";"))
 }
